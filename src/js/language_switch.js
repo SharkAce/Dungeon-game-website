dcba15ca17f789f2aa06493f636f4243497eda24
SharkAce <85720638+SharkAce@users.noreply.github.com>
2022-10-05 14:13:59 -0400

    Made language persistent

--- src/js/language_switch.js
+++ src/js/language_switch.js
@@ -1,62 +1,62 @@
 function loadText(){
 
-    lang = window.location.hash == "#eng" ? "ang" : "fr";
+    lang = window.location.hash == "#eng" ? "eng" : "fr";
+
     switch(document.title){
-        case "description":
-            document.getElementById("description").innerHTML = text.description[lang];
+        case "Description": {
+            document.getElementById("description").innerHTML = bodyText.description[lang];
             break;
+        }
+        case  "Tech": {
+            document.getElementById("tech_cpp").innerHTML = bodyText.tech_cpp[lang];
+            document.getElementById("tech_sfml").innerHTML = bodyText.tech_sfml[lang];
+            document.getElementById("tech_js").innerHTML = bodyText.tech_js[lang];
+            document.getElementById("tech_p5").innerHTML = bodyText.tech_p5[lang];
+            document.getElementById("tech_make").innerHTML = bodyText.tech_make[lang];
+            break;
+        }
+        case "Media": {
+            document.getElementById("media_audio").innerHTML = bodyText.media_audio[lang];
+            document.getElementById("media_video").innerHTML = bodyText.media_video[lang];
+            document.getElementById("media_ytb").innerHTML = bodyText.media_ytb[lang];
+            break;
+        }
+        case "Links": {
+            document.getElementById("link_personal").innerHTML = bodyText.link_personal[lang];
+            document.getElementById("link_libs").innerHTML = bodyText.link_libs[lang];
+        }
+    }
+}
+
+function loadAnchor(){
+
+    lang = window.location.hash == "#eng" ? "eng" : "fr";
+
+    nav_bar = document.getElementById("nav_bar");
+    items = nav_bar.getElementsByTagName("li");
+    itemsArr = [];
+    for (let i = 0; i<items.length-1; i++){
+        itemsArr.push(items.item(i));
+    }
+
+    for (let i=0; i<itemsArr.length; i++){
+        anchor = items[i].getElementsByTagName("a")[0]; 
+
+        if (anchor.href.indexOf('#') != -1){
+            anchor.href = anchor.href.slice(0,anchor.href.indexOf('#'))
+        }
+        anchor.href += document.location.hash;
+
+        anchor.innerHTML = anchorText[i][lang];
+
     }
 }
 
 function switchLanguage(){
     if (window.location.hash != "#eng") window.location.hash = "#eng";
-    else window.location.hash = "";
+    else window.location.hash = "#fr";
 
-    nav_bar = document.getElementById("nav_bar");
-    items = nav_bar.getElementsByTagName("li");
-    items.pop();
 
-    items.forEach(item => {
-        anchor = item.getElementsByTagName("a")[0];
-    });
-    
+    loadAnchor();
     loadText();
-}
-
-const text = {
-    description: {
-        fr:
-        `<b>Description:</b><br>
-        Dungeon game est un jeux 2D en vue de haut que je commencé pour le
-        plaisir et qui est devenue mon projet final pour mon cours de proggramation. <br> <br>
-        <b>Plateforme:</b><br>
-        Linux et Windows<br> <br>
-        <b>Input:</b><br>
-        Clavier et souris<br><br>
-        <b>Temps de jeux:</b><br>
-        15 minutes<br><br>
-        <b>Espace requis:</b><br>
-        5 Mb<br><br>
-        <b>Prix:</b><br>
-        Gratuit<br><br>
-        <b>Langue:</b><br>
-        Anglais<br><br> `,
-        ang:
-        `<b>Description:</b><br>
-        Dungeon game is a game 2D en vue de haut que je commencé pour le
-        plaisir et qui est devenue mon projet final pour mon cours de proggramation. <br> <br>
-        <b>Plateforme:</b><br>
-        Linux et Windows<br> <br>
-        <b>Input:</b><br>
-        Clavier et souris<br><br>
-        <b>Temps de jeux:</b><br>
-        15 minutes<br><br>
-        <b>Espace requis:</b><br>
-        5 Mb<br><br>
-        <b>Prix:</b><br>
-        Gratuit<br><br>
-        <b>Langue:</b><br>
-        Anglais<br><br> `
-    }
-
 }
---
+++ src/js/text.js
@@ -0,0 +1,119 @@
+const anchorText = [
+    {fr: "Principal", eng: "Main"},
+    {fr: "Information", eng: "Description"},
+    {fr: "Tech", eng: "Tech"},
+    {fr: "Editeur", eng: "Editor"},
+    {fr: "Media", eng: "Media"},
+    {fr: "Liens", eng: "Links"}
+];
+
+const bodyText = {
+
+    //description.html
+    description: {
+        fr:
+        `<b>Description:</b><br>
+        Dungeon game est un jeux 2D en vue de haut que je commencé pour le
+        plaisir et qui est devenue mon projet final pour mon cours de proggramation. <br> <br>
+        <b>Plateforme:</b><br>
+        Linux et Windows<br> <br>
+        <b>Périfériques:</b><br>
+        Clavier et souris<br><br>
+        <b>Temps de jeux:</b><br>
+        15 minutes<br><br>
+        <b>Espace requis:</b><br>
+        5 Mb<br><br>
+        <b>Prix:</b><br>
+        Gratuit<br><br>
+        <b>Langue:</b><br>
+        Anglais<br><br> `,
+        eng:
+        `<b>Description:</b><br>
+        Dungeon game is a 2D Dungeon crawler that I started for fun and that later 
+        became my final project for my programming class. <br> <br>
+        <b>Plateforme:</b><br>
+        Linux and Windows<br> <br>
+        <b>Inputs:</b><br>
+        Mouse and Keybord<br><br>
+        <b>Play time:</b><br>
+        15 minutes<br><br>
+        <b>Disk space required:</b><br>
+        5 Mb<br><br>
+        <b>Price:</b><br>
+        Free<br><br>
+        <b>Language:</b><br>
+        English<br><br> `
+    },
+
+    //tech.html
+    tech_cpp: {
+        fr:
+        `J'ai chosis c++ comme langauage pour mon jeux pour plusieurs raison. J'avais déjà experimanter avec du Javascript pour 
+        faire des petits jeux mais pour ce projet je voulais quelque chose de puissant tout en restant orienté object.`,
+        eng:
+        `I chose c++ as the language for my games for several reasons. I had already experimented with JavaScript to
+        make small games but for this project I wanted something powerful while remaining object-oriented.`
+    },
+    tech_sfml: {
+        fr:
+        ``,
+        eng:
+        ``
+    },
+    tech_js: {
+        fr:
+        `Pour l'édteur de tuiles de niveaux présent sur ce site j'ai chosis JavaScript car la performance n'étais pas un avantage et 
+        je voulais revenire a la proggramation de jeux le plus vite possible. J'ai aussi choisi JavaScript pour les froms HTML qui rendais 
+        la tache de parametrer l'éditeur beaucoup plus facile et rapide à implémenter.`,
+        eng:
+        `For the level tile editor present on this site I chose JavaScript because the performance was not an advantage and
+        I wanted to get back to programming the game as soon as possible. I also chose JavaScript for the HTML froms which rendered
+        the task of parameterizing the editor much easier and quicker to implement.`
+    },
+    tech_p5: {
+        fr:
+        ``,
+        eng:
+        ``
+    },
+    tech_make: {
+        fr:
+        ``,
+        eng:
+        ``
+    },
+
+    //video.html
+    media_audio: {
+        fr:
+        `Musique de fond:`,
+        eng:
+        `Background music:`
+    },
+    media_video: {
+        fr:
+        `Demo du jeux:`,
+        eng:
+        `Game demo:`
+    },
+    media_ytb: {
+        fr:
+        `Vidéo tutorielle pour SFML:`,
+        eng:
+        `Tutorial for SFML:`
+    },
+
+    //links.html
+    link_personal: {
+        fr:
+        `Mon code:`,
+        eng:
+        `My code:`
+    },
+    link_libs: {
+        fr:
+        `Libraries utilisées:`,
+        eng:
+        `Used libraries:`
+    }
+};
